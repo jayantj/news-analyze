0f9a6fdaa8ae525c6de38471fee914a4f01500e1
Jayant Jain <jayantjain1992@gmail.com>
2017-12-24 03:50:14 +0530

    More tests for HnCorpus

--- tests/test_article_corpus.py
+++ tests/test_article_corpus.py
@@ -10,7 +10,10 @@ Automated tests for corpuses.
 
 import logging
 import os
+import tempfile
 import unittest
+
+from glob import glob
 
 from news_analyze.utils import HnCorpus as ArticleCorpus
 
@@ -24,21 +27,52 @@ def datapath(fname):
     return os.path.join(module_path, 'data', fname)
 
 
+def testfile():
+    # temporary data will be stored to this file
+    return os.path.join(tempfile.gettempdir(), 'article_corpus.test')
+
+
 class TestArticleCorpus(unittest.TestCase):
 
-    def test_stream_articles_bow(self):
-        data_dir = datapath('articles')
-        corpus = ArticleCorpus(data_dir, metadata={}, min_count=1, max_df=1.0)
-        articles_bow = list(corpus.stream_bow())
+    @classmethod
+    def setUpClass(cls):
+        cls.data_dir = datapath('articles')
+        cls.corpus = ArticleCorpus(cls.data_dir, metadata={}, min_count=1, max_df=1.0)
+        cls.corpus.init_dict()
+
+    def test_stream_bow(self):
+        articles_bow = list(self.corpus.stream_bow())
         self.assertEqual(len(articles_bow), 2)
 
         article_id = articles_bow[0][0]
         self.assertEqual(article_id, 10230628)
 
         word_of_interest = "internet"
-        word_index = corpus.dictionary.token2id[word_of_interest]
+        word_index = self.corpus.dictionary.token2id[word_of_interest]
         word_count = [count for word_id, count in articles_bow[0][1] if word_id == word_index][0]
         self.assertEqual(word_count, 6)
+
+    def test_getitem(self):
+        word_counts = self.corpus[10230628]
+        word_of_interest = "internet"
+        word_index = self.corpus.dictionary.token2id[word_of_interest]
+        word_count = [count for word_id, count in word_counts if word_id == word_index][0]
+        self.assertEqual(word_count, 6)
+
+    def test_cache_for_stream_bow(self):
+        cache_path = testfile()
+        corpus = ArticleCorpus(self.data_dir, metadata={}, min_count=1, max_df=1.0, cache_path=cache_path)
+        self.assertTrue(corpus.cache is None)
+        self.assertFalse(os.path.exists(cache_path))
+
+        corpus.init_dict()
+        self.assertFalse(corpus.cache is None)
+        self.assertTrue(os.path.exists(cache_path))
+
+    @classmethod
+    def tearDownClass(cls):
+        for test_file in glob(testfile() + '*'):
+            os.unlink(test_file)
 
 
 if __name__ == '__main__':
